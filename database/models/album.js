d4c76d4e1689fc3bf73cf4347c36a522b257dfdd
Katie Godinho <katiegodinho@Katies-MBP.ad.gwn>
2020-02-11 17:46:40 -0800

    initial commit of UI for backend project

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,17 @@
 // Todo: create Album Schema
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+});
+
+const Album = mongoose.model("artist", AlbumSchema);
+
+module.exports = Album;
